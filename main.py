d32fdd3a1c00de40952401b7af05d775e1be508c
zakaria.baig <zakaria.baig@engineering.digital.dwp.gov.uk>
2023-06-13 15:02:54 +0100

    add routes

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from website import create_app
 
 app = create_app()
 
+# Condition is satisfied only when main.py is run directly (not satisfied if main.py is imported)
 if __name__ == '__main__':
     app.run(debug=True)
