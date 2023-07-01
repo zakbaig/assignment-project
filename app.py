95ddb17d9229a4adc5c716ee8183307c73022781
zakaria.baig <zakaria.baig@engineering.digital.dwp.gov.uk>
2023-07-01 21:04:16 +0100

    wip refactor

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask_login import LoginManager, login_required, current_user
 from testcontainers.postgres import PostgresContainer
 
 app = Flask(__name__)
-db = SQLAlchemy()
-# DB_NAME = "database.db"
 
 postgres_container = PostgresContainer('postgres:9.5')
 postgres_container.start()
+
+db = SQLAlchemy()
 engine = db.create_engine(postgres_container.get_connection_url())
 
 
@@ -19,11 +19,35 @@ def index():
 
 
 def start():
-    app.config['SECRET_KEY'] = 'afsdadfsasdf'
+    # postgres_container = PostgresContainer('postgres:9.5')
+    # postgres_container.start()
+    print(postgres_container.get_connection_url())
 
+    # db = SQLAlchemy()
+    # engine = db.create_engine(postgres_container.get_connection_url())
+
+    app.config['SECRET_KEY'] = 'afsdadfsasdf'
+    app.config['SQLALCHEMY_DATABASE_URI'] = postgres_container.get_connection_url()
+    db.init_app(app)
+
+    # from website.auth import auth
+    # app.register_blueprint(auth, url_prefix='/')
+
+    with app.app_context():
+        db.create_all()
+        print('Database created!')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        from website.models import Employee
+        return Employee.query.get(int(id))
+
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run()
-    app.config['SQLALCHEMY_DATABASE_URI'] = postgres_container.get_connection_url()
-    db.init_app(app)
+    start()
