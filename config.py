d92bed3c381080aab4754e9a2086829cb73deba8
zakaria.baig <zakaria.baig@engineering.digital.dwp.gov.uk>
2023-07-14 07:46:13 +0100

    feat(AP-10): prepare for deployment

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe(32)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMIN_EMAIL_ADDRESS = os.environ.get('ADMIN_EMAIL_ADDRESS')
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
